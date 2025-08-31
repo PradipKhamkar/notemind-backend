1e6027d52d68486225b7cf36618e497f5447e682
Khamkar Pradip <khamkarpradip20@gmail.com>
2025-08-31 23:03:21 +0530

    added authentication

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,10 @@ app.use(express.json());
 
 import fileRoute from "./routes/file.route";
 import noteRoute from "./routes/note.route";
+import authRoute from "./routes/auth.route";
 
 app.use('/file',fileRoute);
 app.use('/note',noteRoute);
+app.use('/auth',authRoute);
 
 export default app;
