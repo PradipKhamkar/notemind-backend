62d21aacb779a33904523c4dc0bdf8cffcd7d950
Khamkar Pradip <khamkarpradip20@gmail.com>
2025-09-23 23:24:16 +0530

    remove build folder

--- build/helper/file.helper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getFileMetaData = (file) => {
-    return { size: file.size, name: file.name };
-};
-exports.default = { getFileMetaData };
