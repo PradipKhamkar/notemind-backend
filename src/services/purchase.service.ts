72b85bd6c23bee8272d3617851cb624df6023fc2
Khamkar Pradip <khamkarpradip20@gmail.com>
2025-09-27 00:51:01 +0530

    added verify purhcase service

--- src/services/purchase.service.ts
+++ src/services/purchase.service.ts
@@ -2,14 +2,14 @@ import config from "../config";
 import { google } from "googleapis";
 import PurchaseModel from "../models/purchase.model";
 const androidpublisher = google.androidpublisher("v3");
+const packageName = "com.pradip.notemind";
 
 const auth = new google.auth.GoogleAuth({
   credentials: JSON.parse(config.GOOGLE.SERVICE_ACCOUNT as string),
   scopes: ["https://www.googleapis.com/auth/androidpublisher"],
 });
 
-const verifyPurchase = async (
-  packageName: string,
+const verifyPurchaseWithGoogle = async (
   subscriptionId: string,
   purchaseToken: string
 ) => {
@@ -36,29 +36,20 @@ const createPurchase = async (
   planType: "weekly" | "yearly"
 ) => {
   try {
-    const packageName = "com.pradip.notemind";
-    const verifiedInfo = await verifyPurchase(
-      packageName,
+    const verifiedInfo = await verifyPurchaseWithGoogle(
       productId,
       purchaseToken
     );
-    if (!verifiedInfo) throw new Error("failed to verify purchase!");
 
-    const userPurchase = await PurchaseModel.findOne({
-      createdBy: userId,
-      status: "active",
-    });
+    if (!verifiedInfo || !verifiedInfo.expiryTimeMillis) throw new Error("failed to verify purchase!");
+    const { autoRenewing, expiryTimeMillis, startTimeMillis, priceAmountMicros, priceCurrencyCode, } = verifiedInfo;
+    if (parseInt(expiryTimeMillis) <= Date.now()) throw new Error('Purchase Token Expired!');
+
+    const userPurchase = await PurchaseModel.findOne({ createdBy: userId, status: "active" });
     if (userPurchase) {
       userPurchase.status = "expired";
       await userPurchase.save();
     }
-    const {
-      autoRenewing,
-      expiryTimeMillis,
-      startTimeMillis,
-      priceAmountMicros,
-      priceCurrencyCode,
-    } = verifiedInfo;
     const newPurchase = await PurchaseModel.create({
       orderId,
       packageName,
@@ -77,9 +68,31 @@ const createPurchase = async (
     });
     return newPurchase;
   } catch (error) {
-    console.log("Error In Purchase::", JSON.stringify(error));
+    console.log("Error Create In Purchase::", JSON.stringify(error));
     throw error;
   }
 };
+
+const verifyPurchase = async (userId: string, purchaseToken: string) => {
+  try {
+    const purchaseInfo = await PurchaseModel.findOne({ createdBy: userId, purchaseToken: purchaseToken }).select('-packageName');
+    if (!purchaseInfo) throw new Error('Invalid Purchase Token!');
+
+    // verify with google
+    const isValidToken = await verifyPurchaseWithGoogle(purchaseInfo.productId, purchaseToken);
+    if (!isValidToken || !isValidToken.expiryTimeMillis) throw new Error("failed to verify purchase!");
+
+    // check expiry
+    if (parseInt(isValidToken.expiryTimeMillis) <= Date.now()) purchaseInfo.status = "expired";
+    else purchaseInfo.status = "active";
+    await purchaseInfo.save();
+
+    console.log('VerifiedPurchaseInfo::',purchaseInfo);
+    return purchaseInfo
+  } catch (error) {
+    console.log('Error In Verify Purchase',JSON.stringify(error))
+    throw error
+  }
+}
 
 export default { verifyPurchase, createPurchase };
