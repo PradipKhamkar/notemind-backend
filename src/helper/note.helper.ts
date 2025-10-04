6742377bf47b59e6ba21b536f102629761bd3855
Khamkar Pradip <khamkarpradip20@gmail.com>
2025-10-05 02:06:57 +0530

    fixed some issues

--- src/helper/note.helper.ts
+++ src/helper/note.helper.ts
@@ -7,16 +7,17 @@ const checkUserQuota = async (userId: string) => {
    try {
       const userInfo = await UserModel.findById(userId).select("freeQuotaExceed");
       if (!userInfo) throw new Error("user not found");
-
+console.log('console.log(',userInfo.freeQuotaExceed)
       if (userInfo.freeQuotaExceed) {
          const purchase = await purchaseService.verifyPurchase(userId);
          if (purchase?.status !== "active") throw new Error("subscription_need")
-      }
-      const noteCount = await NoteModel.countDocuments({ createdBy: userId });
-      if (noteCount >= config.FREE_NOTE_QUOTA) {
-         userInfo.freeQuotaExceed = true;
-         await userInfo.save();
-         throw new Error("free_quota_exceed");
+      } else {
+         const noteCount = await NoteModel.countDocuments({ createdBy: userId });
+         if (noteCount >= config.FREE_NOTE_QUOTA) {
+            userInfo.freeQuotaExceed = true;
+            await userInfo.save();
+            throw new Error("free_quota_exceed");
+         }
       }
       return true
    } catch (error) {
